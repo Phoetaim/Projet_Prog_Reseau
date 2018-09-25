20820a34c874570263e02f2d5793024300e49857
Lilia Martin <lmrtin027@enseirb-matmeca.fr>
2018-09-25 11:03:17 +0200

    jalon 1

---
+++ src/jalon01/client.c
@@ -0,0 +1,115 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#define STDIN_FILENO 0
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+
+
+// FUNCTIONS
+struct sockaddr_in get_addr_info(char* hostname, int portnumber) {
+  struct sockaddr_in sock_host;
+  memset(& sock_host, '\0', sizeof(sock_host));
+  sock_host.sin_family = AF_INET;
+  sock_host.sin_port = htons(portnumber);
+  inet_aton(hostname, & sock_host.sin_addr);
+  return sock_host;
+}
+
+int do_socket(){
+  int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (sock == -1)
+    perror("do_socket is wrong");
+  return sock;
+}
+
+void do_connect(int sock, struct sockaddr_in address){
+  int connexion = connect(sock, (struct sockaddr *) & address, sizeof(address));
+  if(connexion == -1)
+    perror("do_connect is wrong");
+}
+
+void readline(int fd, char * str, size_t maxlen) {
+  ssize_t count = read(fd, str, maxlen);
+  if(count == -1)
+    perror("readline is wrong");
+  while (count < maxlen) {
+    count += read(fd, str, maxlen-count);
+  }
+}
+
+void sendline(int fd, char * str, size_t maxlen) {
+  ssize_t count = write(fd, str, maxlen);
+  if(count == -1)
+    perror("sendline is wrong");
+  while (count < maxlen) {
+    count += write(fd, str, maxlen-count);
+  }
+}
+
+void handle_client_message(int sock, char * message) {
+  sendline(sock, message, 300);
+}
+
+/* ----------------------------------------------------------------- */
+/* ----------------------------------------------------------------- */
+/* ----------------------------------------------------------------- */
+
+int main(int argc,char** argv)
+{
+
+
+    if (argc != 3)
+    {
+        fprintf(stderr,"usage: RE216_CLIENT hostname port\n");
+        return 1;
+    }
+
+//get address info from the server
+struct sockaddr_in address  = get_addr_info(argv[1], atoi(argv[2]));
+
+
+//get the socket
+int s = do_socket();
+
+//connect to remote socket
+do_connect(s, address);
+
+char* str = malloc(300*sizeof(char));
+ for (;;) {
+  //get user input$
+
+  fgets(str, 300*sizeof(char), stdin);
+
+  //send message to the server
+  handle_client_message(s, str);
+
+
+  if (strncmp(str, "\\quit", 5) == 0) {
+    fprintf(stdout, "coucou");
+    break;
+  }
+
+  //receive response from the server
+   readline(s, str, 300);
+
+   //display the response
+   puts(str);
+
+ }
+
+ close(s);
+ return 0;
+
+
+}
---
+++ src/jalon01/server.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <fcntl.h>
+
+
+// ERRORS
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+//FUNCTIONS
+struct sockaddr_in init_serv_addr(int portnumber) {
+ struct sockaddr_in sock_host;
+ memset(& sock_host, '\0', sizeof(sock_host));
+ sock_host.sin_family = AF_INET;
+ sock_host.sin_port = htons(portnumber);
+ inet_aton("10.7.3.98", & sock_host.sin_addr);
+ return sock_host;
+}
+
+int do_socket() {
+ int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+ if (sock == -1)
+   perror("do_socket is wrong");
+ return sock;
+}
+
+void do_bind(int socketnumber, struct sockaddr_in adr) {
+ int b = bind(socketnumber, (struct sockaddr *) & adr, sizeof(adr));
+ if (b == -1)
+   perror("do_bind is wrong");
+}
+
+int do_listen(int socketnumber, int lognumber) {
+ int l = listen(socketnumber, lognumber);
+ if (l == -1)
+   perror("do_listen is wrong");
+ return l;
+}
+
+int do_accept(int socketnumber, struct sockaddr_in client) {
+  int size = sizeof(struct sockaddr_in);
+  int a = accept(socketnumber, (struct sockaddr *)&client, &size);
+  if (a == -1)
+    perror("do_accept is wrong");
+  return a;
+}
+
+void readline(int fd, char * str, size_t maxlen) {
+  ssize_t count = read(fd, str, maxlen);
+  if(count == -1)
+    perror("readline is wrong");
+  while (count < maxlen) {
+    count += read(fd, str, maxlen-count);
+  }
+}
+
+void sendline(int fd, char * str, size_t maxlen) {
+  ssize_t count = write(fd, str, maxlen);
+  if(count == -1)
+    perror("sendline is wrong");
+  while (count < maxlen) {
+    count += write(fd, str, maxlen-count);
+  }
+}
+
+void do_read(int socketnumber, char * file) {
+  readline(socketnumber, file, 300);
+}
+
+void do_write(int socketnumber, char * file) {
+  sendline(socketnumber, file, 300);
+}
+
+/* ----------------------------------------------------------------- */
+/* ----------------------------------------------------------------- */
+/* ----------------------------------------------------------------- */
+
+int main(int argc, char** argv)
+{
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: RE216_SERVER port\n");
+        return 1;
+    }
+
+
+    //create the socket, check for validity!
+    int s = do_socket();
+
+    //init the serv_add structure
+    struct sockaddr_in address = init_serv_addr(atoi(argv[1]));
+
+    //perform the binding
+    //we bind on the tcp port specified
+    do_bind(s, address);
+
+    //specify the socket to be a server socket and listen for at most 20 concurrent client
+    do_listen(s, 20);
+
+    for (;;)
+    {
+
+        //accept connection from client
+        struct sockaddr_in client;
+        int l = do_accept(s, client);
+        fprintf(stdout, "%d", l);
+
+        //read what the client has to say
+        char* tmp = malloc(300*sizeof(char));
+        do_read(l, tmp);
+
+        if (strncmp(tmp, "\\quit", 5) == 0) {
+          fprintf(stdout, "coucou");
+          close(l);
+        }
+        //we write back to the client
+        do_write(l, tmp);
+
+
+        //clean up client socket
+    }
+
+    //clean up server socket
+    close(s);
+
+    return 0;
+}
